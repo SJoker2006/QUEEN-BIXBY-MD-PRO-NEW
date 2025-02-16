51a314896a63e501a7eb8f382531284727b11ff5
SJoker2006 <lahiruimesh847@gmail.com>
2025-02-16 11:35:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
+global.owner = ['94728380062']
+global.premium = ['94728380062']
+global.ownernomer = 94728380062'
+global.ownername = 'xJOKER'
+global.botname = 'JOKER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
 global.region = 'India, South India, Kerala'
